fb2df31ca9d77f387cc344e9a0805467a2376f0b
Cuong211116 <96030328+Cuong211116@users.noreply.github.com>
2022-01-12 10:54:03 +0900

    a

--- PHPファイル（作成中）/home.php
+++ PHPファイル（作成中）/home.php
@@ -1,8 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja" >
+<head>
+  <meta charset="UTF-8">
+  <title>ホーム</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+    <form action="" method="post">
+        <div class="login">
+            <div class="form">
+            <h2>ホーム</h2>
+            <div class="form-field">
+                <button type="submit" class="button" name="khach">
+                    <div class="arrow-wrapper">
+                    </div>
+                    <p class="button-text">お客様用画面</p>
+                </button>
+                <button type="submit" class="button" name="quanli">
+                    <div class="arrow-wrapper">
+                    </div>
+                    <p class="button-text">管理者用画面</p>
+                </button>
+            </div>
+            </div>
+        </div>
+            </div>
+    </form>
+</body>
+</html>
 <?php
       if(isset($_POST['khach'])){
         header("Location: ./category.php");
   }
       if(isset($_POST['quanli'])){
-          header("Location: ./manager.html");
+          header("Location: ./manager_login.php");
     }																																																			                                                                                                                                                                                                                                                                                                                                                        
 ?>
--- PHPファイル（作成中）/manager.php
+++ PHPファイル（作成中）/manager.php
@@ -1,8 +1,20 @@
 <?php
       if(isset($_POST['home'])){
-        header("Location: ./home.html");
+        header("Location: ./home.php");
   }
       if(isset($_POST['menu'])){
           header("Location: ./category.php");
-    }																																																			                                                                                                                                                                                                                                                                                                                                                        
-?>
+    }	
+    
+        session_start();
+        $username = $_POST['username'] ?? '';
+        $password = $_POST['password'] ?? '';
+            if ( $password == 'supermanager') {
+                $_SESSION['管理者'] = $username;
+                header("Location: ./home.php");
+            } else {
+                $_SESSION['wrong-login'] = 'incorrect username and password';
+                require "./manager_login.php";
+            }
+ 
+?>	

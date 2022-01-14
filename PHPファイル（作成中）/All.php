a36c1c97bcf7e2d508ddea82ef0e13a17672a594
abebe0318 <211040@st.yoshida-g,ac.jp>
2022-01-14 09:12:34 +0900

    a

---
+++ PHPファイル（作成中）/All.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>デストラクタ</title>
+</head>
+<body>
+<?php
+
+    class connect
+    {
+        public $db;
+        // コンストラクタ
+        function __construct(){
+            try {
+                $this->db = new PDO('mysql:host=localhost;dbname=yakibird;charset=utf8', 'root', 'admin');
+                $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo ('データベースに接続できませんでした。' . $e->getMessage());
+                exit();
+            } catch (Exception $e) {
+                echo ('予期せぬエラーです' . $e->getMessage());
+                exit();
+            }
+        }
+        // デストラクタ
+        function __destruct(){
+            $this->db = null;
+        }
+    }
+    ?>
+</body>
+</html>

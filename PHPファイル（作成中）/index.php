12b0fc25d0649e3924ace941fb69d342174d3b97
abebe0318 <211040@st.yoshida-g,ac.jp>
2022-01-25 13:20:47 +0900

    a

--- PHPファイル（作成中）/index.php
+++ PHPファイル（作成中）/index.php
@@ -10,12 +10,59 @@
     ?>
     <style>
         form{
+           
+            
+        }
+        body{
+            background-color :#c0c0c0;
             text-align: center;
+        }
+
+        h1{
+            font-size:25px;
+            color:#191970;
+        }
+
+        .login {
+            /*background-color : #ffdab9;*/
+            width: 500px;
+            margin: 150px auto;
+            font-size: 16px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .login input{
+            box-sizing: border-box;
+            display: block;
+            width: 100%;
+            border-width: 1px;
+            border-style: solid;
+            padding: 11px;
+            font-family: inherit;
+            font-size: 0.95em;
+        }
+
+        /*.login input:hover {
+            text-decoration: none;
+            background-color:#8d8d8d;
+        }*/
+
+
+        .color{
+            background-color :#dcdcdc;
+            height:auto;
+
         }
     </style>
     <form action="" method="post">
     <body>
+    <div class='color'>
+    
+    <div class='login'>
+    <br>
     <h1>ログインページ</h1>
+
     <?php
     if(isset($_POST['check'])){
         $uname = " ";
@@ -51,14 +98,16 @@
    
 
     ?>
-
+    
         ユーザーID：<input typw="text" name="uname" value="" required>
         <br>
         パスワード：<input type="password" name="pass" size="20" value=""  required>
         <br>
         <br>
-        <input type="submit" value="送信" name="check" >
-      
+        <input type="submit" value="ログイン" name="check" >
+        <br>
+        </div> 
+        </div>
     </body>
     </form>
 </html>

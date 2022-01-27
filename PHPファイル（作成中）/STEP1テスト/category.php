0fea273981e44fbae8ff8a54b1187df907e18942
小野寺未来 <211007@st.yoshida-g.ac.jp>
2022-01-27 16:00:58 +0900

    up

--- PHPファイル（作成中）/STEP1テスト/category.php
+++ PHPファイル（作成中）/STEP1テスト/category.php
@@ -111,7 +111,10 @@ if(isset($_POST['manager'])){
     <input type="submit" name="history" value="履歴">
 
     <?php
-    $order_sql = $dbconnect->db-> query('select * from order_table where decition_flag=1 && pay_flag =0');
+    $ini_import = parse_ini_file("terminal.ini", true);
+    $table_no = $ini_import["number"];//注文卓番号
+    
+    $order_sql = $dbconnect->db-> query('select * from order_table where decition_flag=1 && pay_flag =0 && terminal_id ='. $table_no);
     $kakutei_count = $order_sql ->fetch();
     
     //確定の商品があるかどうかで判断

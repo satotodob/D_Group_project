0eeac2a459e06e43c54486d99e2025365a0f7f99
小野寺未来 <211007@st.yoshida-g.ac.jp>
2022-01-27 15:26:16 +0900

    up

--- PHPファイル（作成中）/STEP1テスト/category.php
+++ PHPファイル（作成中）/STEP1テスト/category.php
@@ -109,6 +109,15 @@ if(isset($_POST['manager'])){
 <footer>
 <div class="">
     <input type="submit" name="history" value="履歴">
+    <?php
+    $ini_import = parse_ini_file("terminal.ini", true);
+    $table_no = $ini_import["number"];//注文卓番号
+
+    $order_sql = $dbconnect->db-> query('select * from order_table where decition_flag=1 && terminal_id ='. $table_no);
+    $kakutei_count = $order_sql ->fetch();
+    if($kakutei_count != 0){
+        print('<input type="button" name="goto_pay" onclick="location.href=\'pay.php\'" value="お会計に進む">');
+    }?>
 </div>
 </footer>
 

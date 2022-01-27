0b9a420fc0c89a4b62b1fcbb192c6e3bd59dc339
小野寺未来 <211007@st.yoshida-g.ac.jp>
2022-01-27 10:43:28 +0900

    up

--- PHPファイル（作成中）/STEP1テスト/rireki.php
+++ PHPファイル（作成中）/STEP1テスト/rireki.php
@@ -72,7 +72,7 @@ print "<table border='1'>";
         $order_all = 0;
         $money_all = 0;
     // 確定フラグがtrueで会計フラグがfalseの場合
-    //  $order_sql = $dbconnect->db-> query('select * from order_table where decition_flag=1 && pay_flag =0');
+    $order_sql = $dbconnect->db-> query('select * from order_table where decition_flag=1 && pay_flag =0');
      while($result = $order_sql->fetch()){
         //レコードで取り出した中からカラムを指定して取り出せる
         // print("オーダーidは「".$result['order_id']."」");

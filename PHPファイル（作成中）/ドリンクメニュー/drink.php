36fe65d3a671f232ea07dc1f752a9548d13da80c
kai <211030@st.yoshida-g.ac.jp>
2022-01-28 10:42:47 +0900

    drink

--- PHPファイル（作成中）/ドリンクメニュー/drink.php
+++ PHPファイル（作成中）/ドリンクメニュー/drink.php
@@ -13,6 +13,54 @@
         <?php
             require_once "DB.php";
             $dbconnect = new connect; 
+
+            $ini_import = parse_ini_file("terminal.ini", true);
+            $table_no = $ini_import["number"];//注文卓番号
+        ?>
+
+        <?php
+            //読み込みの関係上、ポストされた情報（ＤＢにインサート等変更を加える処理）は先頭で行います
+
+            if(isset($_POST['order_item']))
+            { //hiddenで設定したメニューid取得
+                $item_id = $_POST['order_item'];
+            }
+
+            if(isset($_POST['item_qty'])){ //radio buttonで選択した数量取得
+                foreach ($_POST['item_qty'] as $optNum => $option) 
+                {
+
+                }
+                //$optionが注文数量
+
+                //インサート処理 
+                $stmt = $dbconnect->db->prepare('insert into order_table values(null, :terminal_no, :menu_id, :quantity, default, default, default)');
+                $stmt->bindValue(':terminal_no',$table_no,PDO::PARAM_INT);
+                $stmt->bindValue(':menu_id',$item_id,PDO::PARAM_INT);
+                $stmt->bindValue(':quantity',$option,PDO::PARAM_INT);
+                $stmt->execute();   
+            }
+
+            if(isset($_POST['del_order'])){ //注文削除ボタン押した場合 order_tableからorder_idを元に削除                          
+                $del = $dbconnect->db->prepare('delete from order_table where order_id = :id');
+                $del->bindValue(':id',$_POST['del_order'],PDO::PARAM_INT);
+                $del->execute();
+            }
+
+            if(isset($_POST['confirm'])){ //注文確定ボタンを押した場合
+                //form actionをkakutei.phpにして、この処理もkakutei.phpに書いた方が良い気もするけど・・・
+                
+                //オーダーテーブルの未確定注文（terminal_id指定）を確定1にupdate
+                $kakutei_order = $dbconnect->db->prepare('update order_table set 
+                                                          decition_flag = 1 
+                                                          where terminal_id = :terminal_no');
+                                    
+                $kakutei_order->bindValue(':terminal_no', $table_no, PDO::PARAM_INT);
+                $kakutei_order->execute();
+                //kakutei.phpに飛ばす↓
+                echo "<script>window.location.href = 'kakutei.php';</script>";
+                exit;
+            }
         ?>
 
         <h1 name="drink">飲み物</h1>
@@ -38,6 +86,7 @@
 
                                     print ('<span>');
                                     print ('<img src="'.$image.'" style="width:294px">');
+                                    print $m_price.'円';
                                     print ('<button type="button" class=drink_img value="'.$image.'" data-toggle="modal" data-target="#'.$menuname.'" style="width:460px; height:197px"></button>');
                                     print ('</span>');?>
 
@@ -54,28 +103,29 @@
                                     
                                                     <div class="modal-body">
                                                         <?php print ('<img src="'.$image.'">'); ?>
-                                                        <input type="hidden" name="order_item" value="<?php print($menuname); ?>">
+                                                        <input type="hidden" name="order_item" value="<?php print($m_id); ?>">
                                                         <input type="hidden" name="item_price" value=<?php print($m_price); ?>>
                                                         <input type="hidden" name="img_price" value=<?php print($image) ; ?>>
+                                                        <input type="hidden" name="item_name" value="<?php print($menuname); ?>">
                                                         
-                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="button" name="item_qty[]" value=1>
-                                                        <label for="qty<?php print($num);?>"></label>
+                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="radio" name="item_qty[]" value=1 checked="checked">
+                                                        <label for="qty<?php print($num);?>">1</label>
                                                         
-                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="button" name="item_qty[]" value=2>
-                                                        <label for="qty<?php print($num);?>"></label>
+                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="radio" name="item_qty[]" value=2>
+                                                        <label for="qty<?php print($num);?>">2</label>
                                                         
-                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="button" name="item_qty[]" value=3>
-                                                        <label for="qty<?php print($num);?>"></label>
+                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="radio" name="item_qty[]" value=3>
+                                                        <label for="qty<?php print($num);?>">3</label>
                                                         
-                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="button" name="item_qty[]" value=4>
-                                                        <label for="qty<?php print($num);?>"></label>
+                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="radio" name="item_qty[]" value=4>
+                                                        <label for="qty<?php print($num);?>">4</label>
                                                         
-                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="button" name="item_qty[]" value=5>
-                                                        <label for="qty<?php print($num);?>"></label>
+                                                        <input id="qty<?php $num= ($num + 1); print($num);?>" type="radio" name="item_qty[]" value=5>
+                                                        <label for="qty<?php print($num);?>">5</label>
                                                     </div>
 
                                                     <div class="modal-footer">
-                                                            <input type="button" class="btn btn-secondary" data-dismiss="modal" style="position:absolute; right:1030px" value="戻る">
+                                                            <input type="button" class="btn btn-secondary" data-dismiss="modal" value="戻る">
                                                             <input type="submit" class="btn btn-primary" value="確定">
                                                     </div>
 
@@ -89,65 +139,50 @@
                 </div>
             </div>
         </div>
+        <br>
 
         <form method="post">
-
-        <div>
+        <div>        
             <table border="1">
                 <tr>
                     <?php
+                        $stmt = $dbconnect->db->query('select COUNT(*) as cnt from order_table where terminal_id = '.$table_no.' and decition_flag = 0');
+                        $order_count = $stmt ->fetchcolumn();
+
                         $img = "";
                         $item = "";
                         $m_price = "";
+                        
+
+                        $order_sql = $dbconnect->db->query('select * from order_table where terminal_id = '.$table_no.' and decition_flag = 0');
                             
-                        if(isset($_POST['img_price']))
-                        { //hiddenで設定した画像取得
-                            $img = $_POST['img_price'];
-                            print ('<th><img src="'.$img.'" style="width:80px; height:50px; "</th>');
+                        if($order_count == 0)
+                        {//未確定注文が0の場合
+                            echo "<tr><td colspan=4>注文はありません。</td></tr>";
                         }
-                        if(isset($_POST['order_item']))
-                        { //hiddenで設定したアイテム名取得
-                            $item = $_POST['order_item'];
-                            print ('<th>"'.print $item.'"</th>');
-                        }
-                        if(isset($_POST['item_price']))
-                        { //hiddenで設定したアイテム単価取得
-                            $m_price = $_POST['item_price'];
-                            //print ('<td>"'.print $m_price.'"</td>');
-                        }
-
-                        if(isset($_POST['item_qty']))
-                        { //radio buttonで選択した数量取得
-                            foreach ($_POST['item_qty'] as $optNum => $option) 
+                        else
+                        {
+                            while($order = $order_sql->fetch())
                             {
+                                $menus = $dbconnect->db->query('select * from menu_table where menu_id = '.$order['menu_id']);
+                                while($menu_item = $menus->fetch())
+                                {
+                                    print ('<th><img src="menuimg/'.$menu_item['menu_name'].'.jpg" style="width:75px; height:55px;"></th>');
+                                
+                                    $item = $menu_item['menu_name'];
+                                    print ('<th>'.$item.'</th>');
 
+                                    $m_price = $menu_item['menu_pay'];
+                                    print ('<th>'.$m_price * $order['quantity'].'¥</th>');
+                            
+                                    print ('<th>'.$order['quantity'].'</th>'); 
+                                    print ('<form method="post" action="">');
+                                    print ('<td><button type="submit" name="del_order" value="'.$order['order_id'].'">注文取消</button></td></tr>');
+                                    print ('</form>');  
+                                } 
                             }
                         }
-
-
-                        // <div id="table">
-                        //     <table border="1">
-                        //         <tr>
-                        //             <th>商品画像</th>    
-                        //             <td>商品名</td>
-                        //             <td>
-                        //                 <select>
-                        //                     <option>注文数</option>
-                        //                     <option>1</option>
-                        //                     <option>2</option>
-                        //                     <option>3</option>
-                        //                     <option>4</option>
-                        //                     <option>5</option>
-                        //                 </select>
-                        //             </td>
-                        //             <td>
-                        //                 <input type="button" name="order_list" value="注文取消">
-                        //             </td>
-                        //         </tr>
-                        //     </table>
-                        // </div>
                     ?>
-                </tr>
             </table>
         </div>
 

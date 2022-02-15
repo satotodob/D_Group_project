84c22f215d87c6cb85296bf00e046e5853bb2d53
kai <211030@st.yoshida-g.ac.jp>
2022-02-15 13:55:39 +0900

    a

--- STEP2cssコーディング用/db_connect.php
+++ STEP2cssコーディング用/db_connect.php
@@ -5,7 +5,7 @@
         // コンストラクタ
         function __construct(){
             try {
-                $this->db = new PDO('mysql:host=localhost;dbname=yakibird;charset=utf8', 'root', '');
+                $this->db = new PDO('mysql:host=localhost;dbname=yakibird;charset=utf8', 'root', 'admin');
                 $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 echo "<script>window.location.href = 'error.php';</script>";
